3803e3f1c2150e3e26b17679592ac2f9f8d5438b
Sangkon Han <sigmadream@gmail.com>
2022-06-08 01:26:00 +0900

    // wip

--- coffee-stores/README.md
+++ coffee-stores/README.md
@@ -2,5 +2,6 @@
 
 ## Ref
 
+- [discover-coffee-stores](https://github.com/kulkarniankita/discover-coffee-stores)
 - [Mesh](https://meshgradient.com/)
 - [MDN - CSS Custom properties](https://developer.mozilla.org/en-US/docs/Web/CSS/Using_CSS_custom_properties)
--- coffee-stores/pages/coffee-store/[id].js
+++ coffee-stores/pages/coffee-store/[id].js
@@ -1,45 +1,104 @@
+import {useRouter} from "next/router";
+import Head from "next/head";
+import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/router";
-
+import cls from "classnames";
+import styles from "../../styles/coffee-store.module.css";
 import coffeeStoresData from "../../data/coffee-stores.json";
 
+
 export function getStaticProps(staticProps) {
-  const params = staticProps.params;
-  return {
-    props: {
-      coffeeStore: coffeeStoresData.find((coffeeStore) => {
-        return coffeeStore.id === 0;
-      }),
-    },
-  };
+    const params = staticProps.params;
+    return {
+        props: {
+            coffeeStores: coffeeStoresData.find((coffeeStore) => {
+                return coffeeStore.id.toString() === params.id;
+            })
+        }
+    }
 }
 
 export function getStaticPaths() {
-  return {
-    paths: [{params: {id: "0"}}, {params: { id: "1"}}],
-    fallback: true
-  }
+    const paths = coffeeStoresData.map((coffeeStore) => {
+        return {
+            params: {
+                id: coffeeStore.id.toString()
+            },
+        };
+    });
+    return {
+        paths, fallback: true
+    }
 }
 
-
 const CoffeeStore = (props) => {
-  const router = useRouter();
-  
-  if (router.isFallback) {
-    return <div>Loading....</div>
-  }
+    const router = useRouter();
 
-  return (
-    <div>
-      Coffee Store Page {router.query.id}
-      <Link href="/"> Back To Home</Link>
-      <Link href="/coffee-store/dynamic">
-        <a>Go to page dynamic</a>
-      </Link>
-      <p>{props.coffeeStore.address}</p>
-      <p>{props.coffeeStore.name}</p>
-    </div>
-  );
+    const {address, name, neighbourhood} = props.coffeeStores;
+
+    if (router.isFallback) {
+        return <div>Loading...</div>;
+    }
+
+    return (<div className={styles.layout}>
+        <Head>
+            <title>{name}</title>
+            <meta name="description" content={`${name} coffee store`}></meta>
+        </Head>
+        <div className={styles.container}>
+            <div className={styles.col1}>
+                <div className={styles.backToHomeLink}>
+                    <Link href="/">
+                        <a>← Back to home</a>
+                    </Link>
+                </div>
+                <div className={styles.nameWrapper}>
+                    <h1 className={styles.name}>{name}</h1>
+                </div>
+                <Image
+                    src={"https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"}
+                    width={600}
+                    height={360}
+                    className={styles.storeImg}
+                    alt={name}
+                ></Image>
+            </div>
+
+            <div className={cls("glass", styles.col2)}>
+                <div className={styles.iconWrapper}>
+                    <Image
+                        src="/static/icons/places.svg"
+                        width="24"
+                        height="24"
+                        alt="places icon"
+                    />
+                    <p className={styles.text}>{address}</p>
+                </div>
+                {neighbourhood && (<div className={styles.iconWrapper}>
+                    <Image
+                        src="/static/icons/nearMe.svg"
+                        width="24"
+                        height="24"
+                        alt="near me icon"
+                    />
+                    <p className={styles.text}>{neighbourhood}</p>
+                </div>)}
+                <div className={styles.iconWrapper}>
+                    <Image
+                        src="/static/icons/star.svg"
+                        width="24"
+                        height="24"
+                        alt="star icon"
+                    />
+                    <p className={styles.text}>55</p>
+                </div>
+
+                <button className={styles.upvoteButton}>
+                    Up vote!
+                </button>
+            </div>
+        </div>
+    </div>);
 };
 
 export default CoffeeStore;
